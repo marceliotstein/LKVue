d7e4fe20129a1709ac411158977d230b9a477c7e
marceliotstein <marc.eliot.stein@gmail.com>
2018-01-13 10:45:57 -0500

    LKQuery component in progress

---
+++ src/components/LKQuery.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="query-results">
+    <LKBurger/>
+    <div class="results-inner">
+      <h1>{{ msg }}</h1>
+      <h2>La Commedia E Finita</h2>
+        <div v-if="processedStories">
+          <ul class="results-ul">
+            <li v-for="item in processedStories">
+              <div class="itemstuff">
+                {{ item }}
+              </div>
+            </li>  
+          </ul>
+        </div>
+      <ul v-if="errors && errors.length">
+        <li v-for="error of errors">
+          {{error.message}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import LKBurger from '@/components/LKBurger.vue';
+
+Vue.use(VueAxios, axios);
+
+export default {
+  name: 'LKQuery',
+  components: {
+     LKBurger
+  },
+  data () {
+    return {
+      msg: 'VueLK: Testing various JSON API filters and data sets.',
+      posts: [],
+      errors: [],
+      results: []
+    }
+  },
+  created() {
+    console.log("IN MOUNTED");
+    this.getStories();
+  }, 
+  methods: {
+    getStories() {
+      console.log("IN GET STORIES");
+      axios.get('http://whatvoir.marceliotstein.net/jsonapi/node/story?sort=-created')
+      .then(response => {
+        console.log("IN GET STORIES - RESPONSE FOUND");
+        this.results = response.data;
+      })
+      .catch(e => {
+        console.log("IN GET STORIES - ERROR");
+        this.errors.push(e)
+      });
+    }
+  },
+  computed: {
+    processedStories() {
+      console.log("IN PROCESSED STORIES");
+      //let storyArray = Array.from(this.results);
+      let storyArray = [ "orange", "apple", "banana" ];
+      console.log(storyArray.toString());
+      return storyArray;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+.query-results {
+  border-width: 1px;
+  border-style: dotted;
+  border-color: orange;
+}
+
+.results-inner {
+  margin-top: 100px;
+}
+
+.results-box {
+  width: 70%;
+  margin: 0 auto;
+  border-width: 3px;
+  border-style: dashed;
+  border-color: purple;
+  text-align: left;
+  padding: 10px;
+  background-color: white;
+  opacity: 50%;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LKPage from '@/components/LKPage'
+import LKQuery from '@/components/LKQuery'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LK Page',
       component: LKPage
-    }
+    },
+    {
+      path: '/querier',
+      name: 'LK Query',
+      component: LKQuery
+    },
   ]
 })
